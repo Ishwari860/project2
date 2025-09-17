240883c18c28ddf54b3bf8572dfb946e488376f0
Ishwari860 <patilishu.1233@gmail.com>
2025-09-17 10:37:28 +0530

    this is my first commmit

---
+++ program2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{ 
+	int*a,i,n;
+	printf("Enter the number of elements:\n");
+	scanf("%d",&n);
+	a=(int*)calloc(n,size of(int));
+	if(a==NULL)
+	{
+		printf("Insufficient memory\n");
+		return;
+	}
+	printf("Enter the N elements:\n");
+	for(i=0;i<n;i++);
+	{
+		scanf("%d",a+i);
+	}
+	printf("The given elements are:\n");
+	for(i=0;i<n;i++);
+	{
+		printf("%d",*(a+i));
+	}
+}
